89427628ddde0cd117e48022989fc5a03f702126
Ragy <ragy@192.168.61.128>
2019-01-14 18:48:50 +0200

    Convert to MVC design pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ragy on 1/14/19.
+//  Copyright © 2019 Ragy. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
